b2f7ea66a6b2710dc7451b1bd35f76e4ad5c77fe
aibenStunner <ebenezergadri99@gmail.com>
2022-05-27 08:16:35 +0000

    Added cli

---
+++ packages/local-api/src/index.ts
@@ -0,0 +1,5 @@
+export const serve = (port: number, filename: string, dir: string) => {
+  console.log("serving on ", port);
+  console.log("serving on ", filename);
+  console.log("serving on ", dir);
+};
